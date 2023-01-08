41c94af13b01574855ada6ae17ebf13e75bdff10
tf <hello@scriptquality.com>
2023-01-08 19:54:23 +0000

    update branch

--- src_backup/index.ts
+++ src_backup/index.ts
@@ -1,3 +1,4 @@
+
 import * as express from 'express';
 import * as bodyParser from 'body-parser';
 import axios from 'axios';
@@ -15,4 +16,13 @@ app.get("/", (req, res) => {
 
 app.listen(port, function () {
     console.log(`Express server listening on port ${port}`)
+})
+
+app.get("/users", (req, res) => {
+    axios.get("https://jsonplaceholder.typicode.com/users")
+        .then(function (response) {
+            res.json(response.data)
+        }).catch(function (error) {
+            res.json("Error occured!")
+        })
 })
